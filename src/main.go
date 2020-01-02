e158ca60ad6ec627f72d6a8bd91bc8efcaca4ad1
JHSeng <zxc98567@gmail.com>
2020-01-02 16:40:37 +0800

    update main.go

--- src/main.go
+++ src/main.go
@@ -1,4 +1,4 @@
-// package main
+package main
 
 // import "fmt"
 
@@ -283,3 +283,23 @@
 // 	test()
 
 // }
+
+// func main() {
+// 	defer func() {
+// 		if err := recover(); err != nil {
+// 			fmt.Println(err)
+// 		}
+// 	}()
+
+// 	defer func() {
+// 		panic("first defer panic")
+// 	}()
+
+// 	defer func() {
+// 		panic("second defer panic")
+// 	}()
+
+// 	panic("main body panic")
+// }
+
+// error
