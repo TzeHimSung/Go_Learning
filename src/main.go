2ea2b37c28ac4be4516e2e2a1ab6e7174f1a9914
JHSeng <zxc98567@gmail.com>
2020-01-14 23:22:12 +0800

    update main.go

--- src/main.go
+++ src/main.go
@@ -20,24 +20,70 @@
 // 	Writer
 // }
 
+// package main
+
+// type printer interface {
+// 	print()
+// }
+
+// type s struct{}
+
+// func (ss s) print() {
+// 	println("print successfully")
+// }
+
+// func main() {
+// 	var i printer // this is an interface example
+// 	// the following code will cause a panic
+// 	// i.print()
+// 	i = s{}   // an interface example must be inited, or it will cause panic
+// 	i.print() // call function print via interface example
+// 	var a s
+// 	a.print() // call function print via struct example
+// }
+
 package main
 
-type printer interface {
-	print()
+import "fmt"
+
+// Inter ...
+type Inter interface {
+	Ping()
+	Pang()
 }
 
-type s struct{}
+// Anter ...
+type Anter interface {
+	Inter
+	String()
+}
 
-func (ss s) print() {
-	println("print successfully")
+// St ...
+type St struct {
+	Name string
+}
+
+// Ping ...
+func (St) Ping() {
+	println("ping")
+}
+
+// Pang ...
+func (*St) Pang() {
+	println("pang")
 }
 
 func main() {
-	var i printer // this is an interface example
-	// the following code will cause a panic
-	// i.print()
-	i = s{}   // an interface example must be inited, or it will cause panic
-	i.print() // call function print via interface example
-	var a s
-	a.print() // call function print via struct example
+	st := &St{"andes"}
+	var i interface{} = st
+	// 判断i绑定的实例是否实现了接口类型Inter
+	o := i.(Inter)
+	o.Ping()
+	o.Pang()
+	// 如下语句会引发panic，因为i没有实现接口Anter
+	// p := i.(Anter)
+	// p.String()
+	// 判断i绑定的实例是否就是具体类型St
+	s := i.(*St)
+	fmt.Printf("%s", s.Name)
 }
