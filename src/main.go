7f3f9cecb98134938f176f128f5e6ca7f1e79421
JHSeng <zxc98567@gmail.com>
2020-01-15 23:07:28 +0800

    update main.go

--- src/main.go
+++ src/main.go
@@ -88,53 +88,148 @@
 // 	fmt.Printf("%s", s.Name)
 // }
 
+// package main
+
+// import "fmt"
+
+// // Inter ...
+// type Inter interface {
+// 	Ping()
+// 	Pang()
+// }
+
+// // Anter ...
+// type Anter interface {
+// 	Inter
+// 	String()
+// }
+
+// // St ...
+// type St struct {
+// 	Name string
+// }
+
+// // Ping ...
+// func (St) Ping() {
+// 	println("ping")
+// }
+
+// // Pang ...
+// func (*St) Pang() {
+// 	println("pang")
+// }
+
+// func main() {
+// 	st := &St{"andes"}
+// 	var i interface{} = st
+// 	// 判断i绑定的实例是否实现了接口类型Inter
+// 	if o, ok := i.(Inter); ok {
+// 		o.Ping()
+// 		o.Pang()
+// 	}
+
+// 	if p, ok := i.(Anter); ok {
+// 		// 由于i没有实现接口Anter，所以程序不会执行到这里
+// 		p.String()
+// 	}
+
+// 	// 判断i绑定的实例是否就是具体类型St
+// 	if s, ok := i.(*St); ok {
+// 		fmt.Printf("%s", s.Name)
+// 	}
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"io"
+// )
+
+// func main() {
+// 	var i io.Reader
+// 	switch v := i.(type) {
+// 	case nil:
+// 		fmt.Printf("%T\n", v)
+// 	default:
+// 		fmt.Printf("default")
+// 	}
+// }
+
+// package main
+
+// import (
+// 	"io"
+// 	"log"
+// 	"os"
+// )
+
+// func main() {
+// 	f, err := os.OpenFile("notes.txt", os.O_RDWR|os.O_CREATE, 0755)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer f.Close()
+// 	var i io.Reader = f
+// 	switch v := i.(type) {
+// 	case io.ReadWriter:
+// 		v.Write([]byte("io.ReadWriter\n"))
+// 	case *os.File:
+// 		v.Write([]byte("*os.File\n"))
+// 		v.Sync()
+// 	default:
+// 		return
+// 	}
+// }
+
+// package main
+
+// import (
+// 	"io"
+// 	"log"
+// 	"os"
+// )
+
+// func main() {
+// 	f, err := os.OpenFile("notes.txt", os.O_RDWR|os.O_CREATE, 0755)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer f.Close()
+// 	var i io.Reader = f
+// 	switch v := i.(type) {
+// 	case *os.File:
+// 		v.Write([]byte("*os.File\n"))
+// 		v.Sync()
+// 	case io.ReadWriter:
+// 		v.Write([]byte("io.ReadWriter\n"))
+// 	default:
+// 		return
+// 	}
+// }
+
 package main
 
-import "fmt"
-
-// Inter ...
-type Inter interface {
-	Ping()
-	Pang()
-}
-
-// Anter ...
-type Anter interface {
-	Inter
-	String()
-}
-
-// St ...
-type St struct {
-	Name string
-}
-
-// Ping ...
-func (St) Ping() {
-	println("ping")
-}
-
-// Pang ...
-func (*St) Pang() {
-	println("pang")
-}
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	st := &St{"andes"}
-	var i interface{} = st
-	// 判断i绑定的实例是否实现了接口类型Inter
-	if o, ok := i.(Inter); ok {
-		o.Ping()
-		o.Pang()
+	f, err := os.OpenFile("notes.txt", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	if p, ok := i.(Anter); ok {
-		// 由于i没有实现接口Anter，所以程序不会执行到这里
-		p.String()
-	}
-
-	// 判断i绑定的实例是否就是具体类型St
-	if s, ok := i.(*St); ok {
-		fmt.Printf("%s", s.Name)
+	defer f.Close()
+	var i io.Reader = f
+	switch v := i.(type) {
+	case *os.File, io.ReadWriter:
+		if v == i {
+			fmt.Println(true)
+		}
+	default:
+		return
 	}
 }
