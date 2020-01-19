7dc76c55b0cc6e30ebbbca42758f3e0ffdc65511
JHSeng <zxc98567@gmail.com>
2020-01-19 19:04:11 +0800

    update main.go

--- src/main.go
+++ src/main.go
@@ -1,96 +1,70 @@
-/*
-segment tree
-implementation with golang
-including signal point fix / query of maxinum element in specific interval
-*/
+// package main
+
+// import (
+// 	"fmt"
+// 	"runtime"
+// 	"time"
+// )
+
+// func main() {
+// 	go func() {
+// 		sum := 0
+// 		for i := 0; i < 1000; i++ {
+// 			sum += i
+// 		}
+// 		fmt.Println(sum)
+// 		time.Sleep(1 * time.Second)
+// 	}()
+// 	// NumGoroutine可以返回当前程序的goroutine数目
+// 	fmt.Println("NumGoroutine =", runtime.NumGoroutine())
+// 	// main goroutine故意sleep几秒防止提前退出
+// 	time.Sleep(3 * time.Second)
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"runtime"
+// 	"time"
+// )
+
+// func sum() {
+// 	sum := 0
+// 	for i := 0; i < 1000; i++ {
+// 		sum += i
+// 	}
+// 	fmt.Println(sum)
+// 	time.Sleep(1 * time.Second)
+// }
+
+// func main() {
+// 	go sum()
+// 	fmt.Println("NumGoroutine =", runtime.NumGoroutine())
+// 	time.Sleep(3 * time.Second)
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"runtime"
+// )
+
+// func main() {
+// 	// 获取当前的GOMAXPROCS值
+// 	fmt.Println("GOMAXPROCS =", runtime.GOMAXPROCS(0))
+// 	// 设置GOMAXPROCS的值为2
+// 	runtime.GOMAXPROCS(2)
+// 	// 获取当前的GOMAXPROCS值
+// 	fmt.Println("GOMAXPROCS =", runtime.GOMAXPROCS(0))
+// }
+
 package main
 
-import "fmt"
-
-// Node node of segment tree
-type Node struct {
-	val int
-}
-
-const maxn int = 1e5 + 10
-
-var segt [maxn]Node
-var a [maxn]int
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-// maintain node of segment tree
-func maintain(curpos int) {
-	segt[curpos].val = max(segt[curpos*2].val, segt[curpos*2+1].val)
-}
-
-// build segment tree
-func build(curpos, curl, curr int) {
-	if curl == curr {
-		segt[curpos].val = a[curl]
-		return
-	}
-	mid := (curl + curr) / 2
-	build(curpos*2, curl, mid)
-	build(curpos*2+1, mid+1, curr)
-	maintain(curpos)
-}
-
-// update value of specific position
-func update(curpos, pos, curl, curr, val int) {
-	if curl == curr {
-		segt[curpos].val = val
-		return
-	}
-	mid := (curl + curr) / 2
-	if pos <= mid {
-		update(curpos*2, pos, curl, mid, val)
-	} else {
-		update(curpos, pos, mid+1, curr, val)
-	}
-	maintain(curpos)
-}
-
-// query maxinum of specific interal
-func queryMax(curpos, curl, curr, ql, qr int) (ret int) {
-	if ql <= curl && curr <= qr {
-		return segt[curpos].val
-	}
-	mid := (curl + curr) / 2
-	if ql <= mid {
-		ret = max(ret, queryMax(curpos*2, curl, mid, ql, qr))
-	}
-	if qr > mid {
-		ret = max(ret, queryMax(curpos*2+1, mid+1, curr, ql, qr))
-	}
-	return
-}
-
 func main() {
-	var n int = 0
-	fmt.Scanf("%d", &n)
-	for i := 1; i <= n; i++ {
-		fmt.Scanf("%d", &a[i])
-	}
-
-	build(1, 1, n)
-
-	var q int = 0
-	fmt.Scanf("%d", &q)
-	for cnt := 1; cnt <= q; cnt++ {
-		var op int = 0
-		var arg1 int = 0
-		var arg2 int = 0
-		fmt.Scanf("%d %d %d", &op, &arg1, &arg2)
-		if op == 1 {
-			update(1, arg1, 1, n, arg2)
-		} else {
-			fmt.Println(queryMax(1, 1, n, arg1, arg2))
-		}
-	}
+	// 创建一个没有缓冲区的通道，通道存放元素类型为datatype
+	make(chan datatype)
+	// 创建一个缓冲区大小为10的通道，通道存放元素类型为datatype
+	make(chan datatype2, 10)
 }
