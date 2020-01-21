270153a1547fb51a9e52eb19819a0f8c3567ca33
JHSeng <zxc98567@gmail.com>
2020-01-21 14:44:59 +0800

    update main.go

--- src/main.go
+++ src/main.go
@@ -290,68 +290,190 @@
 // 	}
 // }
 
+// package main
+
+// import "math/rand"
+
+// import "fmt"
+
+// func generateIntA(done chan struct{}) chan int {
+// 	ch := make(chan int, 5)
+// 	go func() {
+// 	Lable:
+// 		for {
+// 			select {
+// 			case ch <- rand.Int():
+// 			case <-done:
+// 				break Lable
+// 			}
+// 		}
+// 		close(ch)
+// 	}()
+// 	return ch
+// }
+
+// func generateIntB(done chan struct{}) chan int {
+// 	ch := make(chan int, 10)
+// 	go func() {
+// 	Lable:
+// 		for {
+// 			select {
+// 			case ch <- rand.Int():
+// 			case <-done:
+// 				break Lable
+// 			}
+// 		}
+// 		close(ch)
+// 	}()
+// 	return ch
+// }
+
+// func generateInt(done chan struct{}) chan int {
+// 	ch := make(chan int)
+// 	send := make(chan struct{})
+// 	go func() {
+// 	Lable:
+// 		for {
+// 			select {
+// 			case ch <- <-generateIntA(send):
+// 			case ch <- <-generateIntB(send):
+// 			case <-done:
+// 				break Lable
+// 			}
+// 		}
+// 		close(ch)
+// 	}()
+// 	return ch
+// }
+
+// func main() {
+// 	done := make(chan struct{})
+// 	ch := generateInt(done)
+// 	for i := 0; i < 10; i++ {
+// 		fmt.Println(<-ch)
+// 	}
+// 	done <- struct{}{}
+// 	fmt.Println("stop generate")
+// }
+
+// package main
+
+// import "fmt"
+
+// func chain(in chan int) chan int {
+// 	out := make(chan int)
+// 	go func() {
+// 		for v := range in {
+// 			out <- 1 + v
+// 		}
+// 		close(out)
+// 	}()
+// 	return out
+// }
+
+// func main() {
+// 	in := make(chan int)
+// 	go func() {
+// 		for i := 0; i < 10; i++ {
+// 			in <- i
+// 		}
+// 		close(in)
+// 	}()
+// 	// 连续调用三次chain，相当于把in中的每个元素都加3
+// 	out := chain(chain(chain(in)))
+// 	for v := range out {
+// 		fmt.Print(v, " ")
+// 	}
+// }
+
 package main
 
-import "math/rand"
+import (
+	"fmt"
+	"sync"
+)
 
-import "fmt"
-
-func generateIntA(done chan struct{}) chan int {
-	ch := make(chan int, 5)
-	go func() {
-	Lable:
-		for {
-			select {
-			case ch <- rand.Int():
-			case <-done:
-				break Lable
-			}
-		}
-		close(ch)
-	}()
-	return ch
+// 工作任务
+type task struct {
+	begin, end int
+	result     chan<- int
 }
 
-func generateIntB(done chan struct{}) chan int {
-	ch := make(chan int, 10)
-	go func() {
-	Lable:
-		for {
-			select {
-			case ch <- rand.Int():
-			case <-done:
-				break Lable
-			}
-		}
-		close(ch)
-	}()
-	return ch
+// 任务执行：计算begin到end的和
+// 执行结果写入chan result
+func (t *task) do() {
+	sum := 0
+	for i := t.begin; i <= t.end; i++ {
+		sum += i
+	}
+	t.result <- sum
 }
 
-func generateInt(done chan struct{}) chan int {
-	ch := make(chan int)
-	send := make(chan struct{})
-	go func() {
-	Lable:
-		for {
-			select {
-			case ch <- <-generateIntA(send):
-			case ch <- <-generateIntB(send):
-			case <-done:
-				break Lable
-			}
+// 构建task并写入task通道
+func initTask(taskchan chan<- task, r chan int, p int) {
+	qu := p / 10
+	mod := p % 10
+	high := qu * 10
+	for j := 0; j < qu; j++ {
+		b := 10*j + 1
+		e := 10 * (j + 1)
+		tsk := task{
+			begin:  b,
+			end:    e,
+			result: r,
 		}
-		close(ch)
-	}()
-	return ch
+		taskchan <- tsk
+	}
+	// 如果有剩余就再手动加一个
+	if mod != 0 {
+		tsk := task{
+			begin:  high + 1,
+			end:    p,
+			result: r,
+		}
+		taskchan <- tsk
+	}
+	close(taskchan)
+}
+
+// 读取task chan，每个task启动一个worker goroutine进行处理
+// 并等待每个task运行完，关闭结果通道
+func distributeTask(taskchan <-chan task, wait *sync.WaitGroup, result chan int) {
+	for v := range taskchan {
+		wait.Add(1)
+		go processTask(v, wait)
+	}
+	wait.Wait()
+	close(result)
+}
+
+// goroutine处理具体工作，并将处理结果发送到结果通道
+func processTask(t task, wait *sync.WaitGroup) {
+	t.do()
+	wait.Done()
+}
+
+// 读取结果通道，汇总结果
+func processResult(resultchan chan int) int {
+	sum := 0
+	for r := range resultchan {
+		sum += r
+	}
+	return sum
 }
 
 func main() {
-	done := make(chan struct{})
-	ch := generateInt(done)
-	for i := 0; i < 10; i++ {
-		fmt.Println(<-ch)
-	}
-	done <- struct{}{}
-	fmt.Println("stop generate")
+	// 创建任务通道
+	taskchan := make(chan task, 10)
+	// 创建结果通道
+	resultchan := make(chan int, 10)
+	// wait用于同步等待任务的执行
+	wait := &sync.WaitGroup{}
+	// 初始化task的goroutine，计算100个自然数的和
+	go initTask(taskchan, resultchan, 100)
+	// 每个task启动一个goroutine进行处理
+	go distributeTask(taskchan, wait, resultchan)
+	// 通过结果通道获取结果并汇总
+	sum := processResult(resultchan)
+	fmt.Println("sum =", sum)
 }
