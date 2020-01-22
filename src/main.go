993a028b94a3c4388f39aa9f10c1366b1250162f
JHSeng <zxc98567@gmail.com>
2020-01-22 12:00:39 +0800

    update main.go

--- src/main.go
+++ src/main.go
@@ -645,13 +645,40 @@ package main
 
 import "context"
 
+import "time"
+
+import "fmt"
+
 // 定义一个包含Context字段的新类型
 type otherContext struct {
 	context.Context
 }
 
 func work(ctx context.Context, name string) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("%s get msg to cancel\n", name)
+			return
+		default:
+			fmt.Printf("%s is running \n", name)
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
 
+func workWithValue(ctx context.Context, name string) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("%s get msg to cancel\n", name)
+			return
+		default:
+			value := ctx.Value("key").(string)
+			fmt.Printf("%s is running value = %s\n", name, value)
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
 
 func main() {
@@ -659,5 +686,19 @@ func main() {
 	ctxa, cancel := context.WithCancel(context.Background())
 	// work模拟运行并检测前端的退出通知
 	go work(ctxa, "work1")
-	// 使用
+	// 使用withdeadline包装前面的上下文对象ctxa
+	tm := time.Now().Add(3 * time.Second)
+	ctxb, _ := context.WithDeadline(ctxa, tm)
+	go work(ctxb, "work2")
+	// 使用withvalue包装前面的上下文对象ctxb
+	oc := otherContext{ctxb}
+	ctxc := context.WithValue(oc, "key", "andes,pass from main ")
+	go workWithValue(ctxc, "work3")
+	// 故意sleep10秒让work2、work3超时退出
+	time.Sleep(10 * time.Second)
+	// 显式调用work1的cancel方法通知其退出
+	cancel()
+	// 等待work1打印退出信息
+	time.Sleep(5 * time.Second)
+	fmt.Println("main stop")
 }
