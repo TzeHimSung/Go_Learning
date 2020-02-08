f9af8afbe12ba185b8f75784424098d215d39563
JHSeng <zxc98567@gmail.com>
2020-02-08 21:58:26 +0800

    update main.go

--- main/main.go
+++ main/main.go
@@ -65,3 +65,36 @@
 // 	}
 // }
 
+// package main
+
+// import "fmt"
+
+// func nonempty(strings []string) []string {
+// 	i := 0
+// 	for _, s := range strings {
+// 		if s != "" {
+// 			strings[i] = s
+// 			i++
+// 		}
+// 	}
+// 	return strings[:i]
+// }
+
+// func main() {
+// 	fmt.Println(nonempty([]string{"a", "b", "c"}))
+// }
+
+// package main
+
+// import "fmt"
+
+// func remove(slice []int, i int) []int {
+// 	copy(slice[i:], slice[i+1:])
+// 	return slice[:len(slice)-1]
+// }
+
+// func main() {
+// 	s := []int{5, 6, 7, 8, 9}
+// 	fmt.Println(remove(s, 2))
+// }
+
