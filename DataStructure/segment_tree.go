ba2927f13fa974f4568da9acc5828b9bc51dfc8e
TzeHimSung <zxc98567@163.com>
2020-12-21 15:39:18 +0800

    update: datastructure/segmenttree and gocp chapter 7

--- DataStructure/segment_tree.go
+++ DataStructure/segment_tree.go
@@ -12,9 +12,12 @@ type Node struct {
 	val int
 }
 
+// size of source data array
 const maxn int = 1e5 + 10
 
-var segt [maxn]Node
+// definition of segment tree
+var segt [maxn * 4]Node
+// source data array
 var a [maxn]int
 
 func max(a, b int) int {
@@ -26,7 +29,7 @@ func max(a, b int) int {
 
 // maintain node of segment tree
 func maintain(curpos int) {
-	segt[curpos].val = max(segt[curpos*2].val, segt[curpos*2+1].val)
+	segt[curpos].val = max(segt[curpos * 2].val, segt[curpos * 2 + 1].val)
 }
 
 // build segment tree
@@ -36,8 +39,8 @@ func build(curpos, curl, curr int) {
 		return
 	}
 	mid := (curl + curr) / 2
-	build(curpos*2, curl, mid)
-	build(curpos*2+1, mid+1, curr)
+	build(curpos * 2, curl, mid)
+	build(curpos * 2 + 1, mid + 1, curr)
 	maintain(curpos)
 }
 
@@ -49,9 +52,9 @@ func update(curpos, pos, curl, curr, val int) {
 	}
 	mid := (curl + curr) / 2
 	if pos <= mid {
-		update(curpos*2, pos, curl, mid, val)
+		update(curpos * 2, pos, curl, mid, val)
 	} else {
-		update(curpos, pos, mid+1, curr, val)
+		update(curpos, pos, mid + 1, curr, val)
 	}
 	maintain(curpos)
 }
@@ -63,28 +66,28 @@ func queryMax(curpos, curl, curr, ql, qr int) (ret int) {
 	}
 	mid := (curl + curr) / 2
 	if ql <= mid {
-		ret = max(ret, queryMax(curpos*2, curl, mid, ql, qr))
+		ret = max(ret, queryMax(curpos * 2, curl, mid, ql, qr))
 	}
 	if qr > mid {
-		ret = max(ret, queryMax(curpos*2+1, mid+1, curr, ql, qr))
+		ret = max(ret, queryMax(curpos * 2 + 1, mid + 1, curr, ql, qr))
 	}
 	return
 }
 
 func main() {
 	var n int = 0
-	fmt.Scanf("%d", &n)
+	fmt.Scan(&n)
 	for i := 1; i <= n; i++ {
-		fmt.Scanf("%d", &a[i])
+		fmt.Scan(&a[i])
 	}
 
 	build(1, 1, n)
 
 	var q int = 0
-	fmt.Scanf("%d", &q)
+	fmt.Scan(&q)
 	for cnt := 1; cnt <= q; cnt++ {
 		var op, arg1, arg2 = 0, 0, 0
-		fmt.Scanf("%d %d %d", &op, &arg1, &arg2)
+		fmt.Scan(&op, &arg1, &arg2)
 		if op == 1 {
 			update(1, arg1, 1, n, arg2)
 		} else {
